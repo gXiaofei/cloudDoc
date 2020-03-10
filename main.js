64c887619313470e3c758564cf6cc79fc59c86c7
gxf <15180000696@163.com>
2020-03-11 00:03:00 +0800

    update

--- main.js
+++ main.js
@@ -8,22 +8,88 @@ const {
 } = require('electron');
 const isDev = require('electron-is-dev');
 const path = require('path');
+const { autoUpdater, CancellationToken } = require('electron-updater');
 const menuTemplate = require('./src/menuTemplate');
 const AppWindow = require('./src/AppWindow');
-const Store = require('electron-store')
-const QiniuManager = require('./src/utils/QiniuManager')
+const Store = require('electron-store');
+const QiniuManager = require('./src/utils/QiniuManager');
 const settingsStore = new Store({ name: 'Settings' });
 const filesStore = new Store({ name: 'Files Data' });
 let mainWindow, settingsWindow;
 
-
 const createManager = () => {
-    const accessKey = settingsStore.get('accessKey')
-    const secretKey = settingsStore.get('secretKey')
-    const bucketName = settingsStore.get('bucketName')
-    return new QiniuManager(accessKey, secretKey, bucketName)
-}
+    const accessKey = settingsStore.get('accessKey');
+    const secretKey = settingsStore.get('secretKey');
+    const bucketName = settingsStore.get('bucketName');
+    return new QiniuManager(accessKey, secretKey, bucketName);
+};
 function createWindow() {
+    const cancellationToken = new CancellationToken();
+    if (isDev) {
+        autoUpdater.updateConfigPath = path.join(
+            __dirname,
+            'dev-app-update.yml'
+        );
+    }
+
+    autoUpdater.autoDownload = false;
+    autoUpdater.checkForUpdates();
+    autoUpdater.on('error', error => {
+        dialog.showErrorBox(
+            'Error: ',
+            error == null ? 'unknown' : (error.stack || error).toString()
+        );
+    });
+    autoUpdater.on('checking-for-update', () => {
+        console.log('Checking for update...');
+    });
+    autoUpdater.on('update-available', () => {
+        dialog.showMessageBox(
+            {
+                type: 'info',
+                title: '应用有新的版本',
+                message: '发现新版本，是否现在更新?',
+                buttons: ['是', '否']
+            },
+            buttonIndex => {
+                if (buttonIndex === 0) {
+                    autoUpdater.downloadUpdate(cancellationToken);
+                }
+            }
+        );
+    });
+    // 取消下载
+    // cancellationToken.cancel()
+    autoUpdater.on('update-not-available', () => {
+        dialog.showMessageBox({
+            title: '没有新版本',
+            message: '当前已经是最新版本'
+        });
+    });
+    autoUpdater.on('download-progress', progressObj => {
+        let log_message = 'Download speed: ' + progressObj.bytesPerSecond;
+        log_message =
+            log_message + ' - Downloaded ' + progressObj.percent + '%';
+        log_message =
+            log_message +
+            ' (' +
+            progressObj.transferred +
+            '/' +
+            progressObj.total +
+            ')';
+        console.log(log_message);
+    });
+    autoUpdater.on('update-downloaded', () => {
+        dialog.showMessageBox(
+            {
+                title: '安装更新',
+                message: '更新下载完毕，应用将重启并进行安装'
+            },
+            () => {
+                setImmediate(() => autoUpdater.quitAndInstall());
+            }
+        );
+    });
 
     const mainWindowConfig = {
         width: 1200,
@@ -33,7 +99,9 @@ function createWindow() {
     // and load the index.html of the app.
     // mainWindow.loadFile('index.html');
 
-    const urlLocation = isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, './index.html')}`;
+    const urlLocation = isDev
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, './index.html')}`;
     // mainWindow.loadURL(urlLocation);
     mainWindow = new AppWindow(mainWindowConfig, urlLocation);
     mainWindow.on('closed', () => {
@@ -65,41 +133,58 @@ function createWindow() {
     // 同步上传七牛云
     ipcMain.on('upload-file', (event, data) => {
         const manager = createManager();
-        manager.uploadFile(data.key, data.path).then(data => {
-            console.log('上传成功', data);
-            mainWindow.webContents.send('active-file-uploaded')
-        }).catch(err => {
-            dialog.showErrorBox('同步失败', '请检查七牛云同步不参数')
-        })
-    })
+        manager
+            .uploadFile(data.key, data.path)
+            .then(data => {
+                console.log('上传成功', data);
+                mainWindow.webContents.send('active-file-uploaded');
+            })
+            .catch(err => {
+                dialog.showErrorBox('同步失败', '请检查七牛云同步不参数');
+            });
+    });
     ipcMain.on('download-file', (event, data) => {
         const manager = createManager();
-        const filesObj = filesStore.get('files')
-        const {key, path, id} = data;
+        const filesObj = filesStore.get('files');
+        const { key, path, id } = data;
         console.log(data);
-        manager.getStat(key).then(resp => {
-            console.log(filesObj[id]);
-            const serverUpdatedTime = Math.round(resp.putTime / 10000);
-            const locationUpdatedTime = filesObj[id].uploadedAt;
-            console.log(serverUpdatedTime)
-            console.log(locationUpdatedTime)
-            if(!locationUpdatedTime || serverUpdatedTime > locationUpdatedTime){
-                console.log('new file')
-                manager.downloadFile(key, path).then(() => {
-                    mainWindow.webContents.send('file-downloaded', {status: 'download-success', id})
-                })
-            } else {
-                console.log('no new file')
-                mainWindow.webContents.send('file-downloaded', {status: 'no-new-file', id})
+        manager.getStat(key).then(
+            resp => {
+                console.log(filesObj[id]);
+                const serverUpdatedTime = Math.round(resp.putTime / 10000);
+                const locationUpdatedTime = filesObj[id].uploadedAt;
+                console.log(serverUpdatedTime);
+                console.log(locationUpdatedTime);
+                if (
+                    !locationUpdatedTime ||
+                    serverUpdatedTime > locationUpdatedTime
+                ) {
+                    console.log('new file');
+                    manager.downloadFile(key, path).then(() => {
+                        mainWindow.webContents.send('file-downloaded', {
+                            status: 'download-success',
+                            id
+                        });
+                    });
+                } else {
+                    console.log('no new file');
+                    mainWindow.webContents.send('file-downloaded', {
+                        status: 'no-new-file',
+                        id
+                    });
+                }
+            },
+            error => {
+                console.log(error);
+                if (error.statusCode === 612) {
+                    mainWindow.webContents.send('file-downloaded', {
+                        status: 'no-file',
+                        id
+                    });
+                }
             }
-
-        }, (error) => {
-            console.log(error);
-            if(error.statusCode === 612){
-                mainWindow.webContents.send('file-downloaded', {status: 'no-file', id})
-            }
-        })
-    })
+        );
+    });
 
     ipcMain.on('upload-all-to-qiniu', () => {
         mainWindow.webContents.send('loading-status', true);
@@ -109,22 +194,25 @@ function createWindow() {
         const uploadPromiseArr = Object.keys(filesObj).map(key => {
             const file = filesObj[key];
             return manager.uploadFile(`${file.title}.md`, file.path);
-        })
+        });
 
-        Promise.all(uploadPromiseArr).then(res => {
-            console.log(res)
-            dialog.showMessageBox({
-                type: 'info',
-                title: `成功上传了${res.length}个文件`,
-                message: `成功上传了${res.length}个文件`,
+        Promise.all(uploadPromiseArr)
+            .then(res => {
+                console.log(res);
+                dialog.showMessageBox({
+                    type: 'info',
+                    title: `成功上传了${res.length}个文件`,
+                    message: `成功上传了${res.length}个文件`
+                });
+                mainWindow.webContents.send('files-uploaded');
             })
-            mainWindow.webContents.send('files-uploaded');
-        }).catch(() => {
-            dialog.showErrorBox('同步失败', '请检查七牛云同步不参数')
-        }).finally(() => {
-            mainWindow.webContents.send('loading-status', false);
-        })
-    })
+            .catch(() => {
+                dialog.showErrorBox('同步失败', '请检查七牛云同步不参数');
+            })
+            .finally(() => {
+                mainWindow.webContents.send('loading-status', false);
+            });
+    });
 
     // 动态菜单
     ipcMain.on('config-is-saved', () => {
